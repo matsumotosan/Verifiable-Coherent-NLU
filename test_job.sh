db43feb0ab71756725310342b431030f38dfa993
Shion <smatsumoto97@gmail.com>
2022-11-15 22:26:39 -0500

    Add job script

---
+++ test_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=eecs595f22_class
+#SBATCH --partition=gpu
+#SBATCH --time=00-03:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-gpu=20
+#SBATCH --mem-per-gpu=90G
+#SBATCH --account=eecs595f22_class
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# set up job
+module load python/3.9.12 cuda
+pushd /home/matsumos/Verifiable-Coherent-NLU
+source /venv/bin/activate
+
+# run job
+python3 main.py
